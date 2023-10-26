c48f70937e4fccf961fb8a658890f34805594c70
Mudi26 <s215772@student.dtu.dk>
2023-10-26 17:46:39 +0200

    Added many methods to the GamePanel class, also an Inner class, that has the keyPressed method.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,14 +2,55 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener { //JPanel acts as CSS (In the HTML - CSS relationship)
 
     GamePanel(){ //This is a constructor of the GamePanel class
         //setBackground(Color.black);
     }
+
+    public void startGame(){ //Method for starting game
+
+    }
+
+    public void paintComponent(Graphics g){ //Method for coloring?
+
+    }
+
+    public void draw(Graphics g){//Method for coloring?
+
+    }
+
+    public void move(){ //Method for moving
+
+    }
+
+    public void checkApple(){ //Method for checking if apple is present?
+
+    }
+
+    public void checkCollisions(){ //Method for if snake hits something it shouldn't
+
+    }
+
+    public void gameOver(Graphics g){ //Method for game over
+
+    }
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{ //This is an inner class
+        @Override
+        public void keyPressed(KeyEvent e){ //when pressing a key, this method is used
+
+        }
 
     }
 }
