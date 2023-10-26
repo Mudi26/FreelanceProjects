28290b0e1fd822273787fb96c3269db96055948b
Mudi26 <s215772@student.dtu.dk>
2023-10-26 16:58:07 +0200

    Testing the debugger, with a little addition program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! This is a test");
+        System.out.println("What is i");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+
+        if ((i + 2) == 5){
+            System.out.println("it is 5, i was "+i);
+        } else {
+            System.out.println("it is not 5");
+        }
     }
 }
